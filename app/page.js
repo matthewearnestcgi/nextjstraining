4f789d4db463569badcde0fead9442b39028b1cd
Earnest <matthew.earnest@cgi.com>
2024-07-19 12:58:31 -0500

    added dependencies

--- app/page.js
+++ app/page.js
@@ -3,11 +3,9 @@ import RootLayout from "/app/layout";
 
 export default function Home() {
   return (
-         <RootLayout>
-            <div>
-                <h1>Welcome to my Next.js App</h1>
-                <p>This app demonstrates various functionalities.</p>
-            </div>
-        </RootLayout>
+        <div>
+            <h1>Welcome to my Next.js App</h1>
+            <p>This app demonstrates various functionalities.</p>
+        </div>
   );
 }
